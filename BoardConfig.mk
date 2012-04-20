a2924713e45b34f6a38349668c786d3bc891a9b0
HD <hdvision16@gmail.com>
2012-04-19 19:25:21 -0500

    Board configuration makefile

---
+++ BoardConfig.mk
@@ -0,0 +1,109 @@
+# inherit from the proprietary version
+-include vendor/bn/acclaim/BoardConfigVendor.mk
+
+USE_CAMERA_STUB := true
+BOARD_USES_GENERIC_AUDIO := false
+
+# Board props
+TARGET_NO_BOOTLOADER := false
+TARGET_BOARD_PLATFORM := omap4
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_BOOTLOADER_BOARD_NAME := omap4sdp
+TARGET_CPU_SMP := true
+TARGET_PROVIDES_INIT_RC := true
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_ARCH_VARIANT_FPU := neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+NEEDS_ARM_ERRATA_754319_754320 := true
+TARGET_GLOBAL_CFLAGS += -DNEEDS_ARM_ERRATA_754319_754320
+BOARD_NEEDS_CUTILS_LOG := true
+
+BOARD_KERNEL_CMDLINE := androidboot.console=ttyO0 console=ttyO0,115200n8 mem=448M@0x80000000 mem=512M@0xA0000000 init=/init rootwait vram=32M,82000000 omapfb.vram=0:5M@82000000
+BOARD_KERNEL_BASE := 0x80080000
+BOARD_KERNEL_PAGESIZE := 4096
+
+# Graphics
+BOARD_EGL_CFG := device/bn/acclaim/misc/egl.cfg
+
+# HW Graphics
+USE_OPENGL_RENDERER := true
+
+# Packaging
+TARGET_PROVIDES_RELEASETOOLS := true
+TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := device/bn/acclaim/releasetools/acclaim_img_from_target_files
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/bn/acclaim/releasetools/acclaim_ota_from_target_files
+
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# OMAP
+OMAP_ENHANCEMENT := true
+ENHANCED_DOMX := true
+
+ifdef OMAP_ENHANCEMENT
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT -DTARGET_OMAP4
+endif
+
+# Fix this up by examining /proc/partitions on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 641728512
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12949893120
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+BOARD_HAVE_BLUETOOTH := false
+BOARD_HAVE_FAKE_GPS := true
+BOARD_GPS_LIBRARIES := false
+TARGET_NO_RADIOIMAGE := true
+USE_CAMERA_STUB := true
+BOARD_USES_ALSA_AUDIO := true
+BUILD_WITH_ALSA_UTILS := true
+BOARD_USES_TI_OMAP_MODEM_AUDIO := false
+BOARD_HAS_NO_MISC_PARTITION := true
+
+# OMX
+HARDWARE_OMX := true
+ifdef HARDWARE_OMX
+OMX_JPEG := true
+OMX_VENDOR := ti
+OMX_VENDOR_INCLUDES := \
+  hardware/ti/omx/system/src/openmax_il/omx_core/inc \
+  hardware/ti/omx/image/src/openmax_il/jpeg_enc/inc
+OMX_VENDOR_WRAPPER := TI_OMX_Wrapper
+BOARD_OPENCORE_LIBRARIES := libOMX_Core
+BOARD_OPENCORE_FLAGS := -DHARDWARE_OMX=1
+BOARD_CAMERA_LIBRARIES := libcamera
+endif
+
+# XXX: CHANGED FROM TRUE
+BOARD_USES_TI_CAMERA_HAL := false
+
+TARGET_PREBUILT_KERNEL := device/bn/acclaim/kernel
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/bn/acclaim/recovery/recovery_ui.c
+
+# Use pre-kernel.35 vold usb mounting
+BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+
+# Storage
+BOARD_HAS_SDCARD_INTERNAL := true
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/platform/mmci-omap-hs.1/by-name/media
+BOARD_SDCARD_DEVICE_INTERNAL := /dev/block/platform/mmci-omap-hs.1/by-name/media
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+
+# Wifi
+BOARD_WPA_SUPPLICANT_DRIVER := CUSTOM
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := libCustomWifi
+WPA_SUPPLICANT_VERSION      := VER_0_6_X
+HOSTAPD_VERSION             := VER_0_6_X
+BOARD_WLAN_DEVICE           := wl1283
+BOARD_SOFTAP_DEVICE         := wl1283
+WIFI_DRIVER_MODULE_PATH     := "/system/etc/wifi/tiwlan_drv.ko"
+WIFI_DRIVER_MODULE_NAME     := "tiwlan_drv"
+WIFI_DRIVER_MODULE_ARG      := ""
+WIFI_FIRMWARE_LOADER        := "wlan_loader"
+WIFI_DRIVER_FW_STA_PATH     := "/system/etc/wifi/firmware.bin"
+WIFI_DRIVER_FW_AP_PATH      := "/system/etc/wifi/softap/firmware_ap.bin"
+
+# Use this flag if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
