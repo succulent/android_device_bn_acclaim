c016eb46e2a77f7381402c0d80707c1b73d590d5
HD <hdvision16@gmail.com>
2012-07-01 10:49:20 -0500

    recovery stuffs

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -1,8 +1,9 @@
 #!/sbin/sh
 
-# Resets the boot counter and the bcb instructions
+# Resets the boot counter and the BCB instructions
 mkdir /bootdata
 mount /dev/block/mmcblk0p6 /bootdata
+mount -o rw,remount /bootdata
 
 # Zero out the boot counter
 dd if=/dev/zero of=/bootdata/BootCnt bs=1 count=4
@@ -10,7 +11,5 @@ dd if=/dev/zero of=/bootdata/BootCnt bs=1 count=4
 # Reset the bootloader control block (bcb) file
 dd if=/dev/zero of=/bootdata/BCB bs=1 count=1088
 
-
 umount /bootdata
 rmdir /bootdata
-
