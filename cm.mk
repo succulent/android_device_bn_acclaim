04616434b81a75865094a015e0844bb0fd7862dc
HD <hdvision16@gmail.com>
2012-12-16 13:02:44 -0600

    switch width/height for bootanimation

--- cm.mk
+++ cm.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 #
 
-TARGET_SCREEN_WIDTH := 1024
-TARGET_SCREEN_HEIGHT := 600
+TARGET_SCREEN_WIDTH := 600
+TARGET_SCREEN_HEIGHT := 1024
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
