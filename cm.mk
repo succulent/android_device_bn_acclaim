96be12f88ed7a37537e532b7bd40f4657dfc0dbf
HD <hdvision16@gmail.com>
2013-01-07 08:54:30 -0600

    cm.mk reverted # Bootanimation

--- cm.mk
+++ cm.mk
@@ -14,6 +14,11 @@
 # limitations under the License.
 #
 
+
+# Bootanimation
+TARGET_SCREEN_WIDTH := 600
+TARGET_SCREEN_HEIGHT := 1024
+
 # Inherit some common CM stuff.
 $(call inherit-product-if-exists, vendor/cm/config/common_full_tablet_wifionly.mk)
 
