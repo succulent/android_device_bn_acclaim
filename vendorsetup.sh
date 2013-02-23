75b5ee840eb7babe47a9dc849d1e8477796388e8
HD <hdvision16@gmail.com>
2013-02-23 14:47:05 -0600

    setup for Xylon

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,6 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_acclaim-eng
-add_lunch_combo cm_acclaim-eng
-add_lunch_combo aokp_acclaim-eng
+add_lunch_combo full_acclaim-userdebug
+add_lunch_combo cm_acclaim-userdebug
+add_lunch_combo aokp_acclaim-userdebug
